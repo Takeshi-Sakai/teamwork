5e2311107d0cf09f17b861104a3786007bea3a43
Takeshi Sakai <s.takeshi16@gmail.com>
2017-05-12 15:16:34 +0900

    initial commit

---
+++ cms/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from cms.models import Improvement, HH
+
+# Register your models here.
+admin.site.register(Improvement)
+admin.site.register(HH)
---
+++ cms/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+class Improvement(models.Model):
+    """改善"""
+    name = models.CharField('名前', max_length=255)
+    subject = models.CharField('件名', max_length=255)
+    done_date = models.DateField('処置完了日')
+    boss_confirmation = models.DateField('上司確認')
+
+    def __str__(self):
+        return self.name
+
+
+class HH(models.Model):
+    """ヒヤリハット"""
+    hh = models.ForeignKey(Improvement, verbose_name='改善', related_name='hhat')
+    comment = models.TextField('内容', blank=True)
+
+    def __str__(self):
+        return self.comment
